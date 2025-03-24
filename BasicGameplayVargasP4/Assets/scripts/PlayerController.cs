758aac25808ed563fae947a2a33fed51bff3f4c0
cogito17 <729426@student.dusd.net>
2025-03-24 11:58:23 -0700

    .
    .

--- BasicGameplayVargasP4/Assets/scripts/PlayerController.cs
+++ BasicGameplayVargasP4/Assets/scripts/PlayerController.cs
@@ -6,7 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
-    public float xRange = -10;
+    public float xRange = 10;
+
+    public GameObject projectilePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -18,9 +20,9 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Keep the player in bounds
-        if (transform.position.x < xRange) 
+        if (transform.position.x < -xRange) 
         {
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
         if (transform.position.x > xRange)
         {
@@ -32,7 +34,10 @@ public class PlayerController : MonoBehaviour
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
 
+
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
     }
 }
